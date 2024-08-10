701c4a2f6420914ba9307b7852b36035b017c015
knsxw <letsmail.khinekhant@gmail.com>
2024-08-10 22:16:42 +0700

    Assignment2

---
+++ Assignment2/Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+    String name;
+    int age;
+    String species;
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Species: " + species);
+    }
+}
---
+++ Assignment2/Main.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static ArrayList<Animal> animals = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        String flag;
+        do {
+            inputRequest();
+            System.out.print("Do you want to add new animal? (yes/no): ");
+            flag = br.readLine();
+        } while (flag.equalsIgnoreCase("yes"));
+        displayAnimals();
+    }
+
+    public static void displayAnimals() {
+        System.out.println();
+        System.out.println("Total number of animals: " + animals.size());
+        System.out.println();
+        int i = 1;
+        for (Animal animal : animals) {
+            System.out.println("Animal " + i + ": " );
+            animal.display();
+            i++;
+            System.out.println();
+        }
+    }
+
+    public static void inputRequest() throws IOException {
+        Animal animal = new Animal();
+        System.out.print("Enter the name of the animal: ");
+        animal.name = br.readLine();
+        System.out.print("Enter the age of the animal: ");
+        animal.age = Integer.parseInt(br.readLine());
+        System.out.print("Enter the species of the animal: ");
+        animal.species = br.readLine();
+        animals.add(animal);
+    }
+}
